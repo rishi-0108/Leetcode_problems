2a7c820f245d45cb7cfc49c9fb7b5ad60b333e95
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-08-30 16:50:35 +0530

    Time: 2 ms (62.41%), Space: 118 MB (64.26%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -11,6 +11,10 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
+        /*
+
+        brute force - o(n) sc
+        
         stack<int> st;
         ListNode* temp=head;
         while(temp != nullptr){
@@ -21,6 +25,34 @@ public:
         while(temp != nullptr){
             if(temp->val != st.top())return false;
             st.pop();
+            temp=temp->next;
+        }
+        return true;
+        
+        */
+
+        //optimal : o(1) sc : use slow fast to find mid then reverse from mid then compare 1st half and second half
+
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast != nullptr && fast->next != nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* prev=nullptr;
+        ListNode* curr=slow;
+        while(curr != nullptr){
+            ListNode* next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+
+        // comparison begins
+        ListNode* temp=head;
+        while(prev != nullptr){
+            if(prev->val != temp->val)return false;
+            prev=prev->next;
             temp=temp->next;
         }
         return true;
