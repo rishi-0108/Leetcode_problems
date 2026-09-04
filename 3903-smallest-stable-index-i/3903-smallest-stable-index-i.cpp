a0df5bc76c5b0caf95ca05c9b9c2a7449fed9c40
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-09-04 16:31:26 +0530

    Time: 0 ms (100%), Space: 31.5 MB (36.87%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
+        /*
+        brute force :
         int maxi=INT_MIN;
-        // int stable=INT_MAX;
         int ind=-1;
         for(int i=0;i<nums.size();i++){
             int mini=INT_MAX;
@@ -11,9 +12,26 @@ public:
             }
             maxi=max(maxi,nums[i]);
             if((maxi-mini)<=k){
-                // stable=maxi-mini;
                 return i;
             }
+        }
+        return -1;
+        */
+
+        // optimal prefix sum 
+        int n=nums.size();
+        vector<int> mini(n),maxi(n);
+        mini[n-1]=nums[n-1];
+        maxi[0]=nums[0];
+        for(int i=1;i<n;i++){
+            maxi[i]=max(maxi[i-1],nums[i]);
+        }
+        for(int i=n-2;i>=0;i--){
+            mini[i]=min(mini[i+1],nums[i]);
+        }
+        for(int i=0;i<n;i++){
+            if((maxi[i]-mini[i])<=k)
+            return i;
         }
         return -1;
     }
